4b5df19c283e150eae019467a3e0562d3984b301
Alexander Nenninger <alexander.nenninger@fu-berlin.de>
2021-11-14 19:30:46 +0100

    add color enums

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,111 @@
+#![allow(dead_code)]
+
 extern crate image;
 extern crate ndarray;
 
 use ndarray::prelude::*;
 
 mod viz;
-use viz::{array_to_image, quadrant};
+#[allow(unused)]
+use viz::array_to_image;
 mod newton;
 use newton::*;
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cluster {
+    tol: f64,
+    cluster: Vec<Complex64>,
+    count: Vec<usize>,
+}
+
+impl Cluster {
+    fn new() -> Self {
+        Self {
+            tol: 0.01,
+            cluster: vec![],
+            count: vec![],
+        }
+    }
+
+    // Update count and mean for z is close to a previous cluster or make a new cluster.
+    fn push(&mut self, z: Complex64) -> usize {
+        let mut i = 0;
+        for (cls, count) in self.cluster.iter_mut().zip(self.count.iter_mut()) {
+            if (z - *cls).norm() < self.tol {
+                *cls = (*cls * (*count as f64) + z) / (*count + 1) as f64;
+                *count += 1;
+                return i;
+            }
+            i += 1
+        }
+        self.cluster.push(z);
+        self.count.push(1);
+        return i;
+    }
+
+    fn push_maybe(&mut self, z: Option<Complex64>) -> isize {
+        match z {
+            Some(z) => self.push(z) as isize,
+            None => -1,
+        }
+    }
+}
+
 fn main() {
-    let s = (9999, 9999, 3);
+    let s = (10, 10);
     let min_z = Complex64::new(-1., -1.);
     let max_z = Complex64::new(1., 1.);
 
-    let mut out = Array3::<u8>::zeros(s);
+    let mut roots = Cluster::new();
+    let mut root_img = Array2::<isize>::zeros(s);
 
-    let f = |x: Complex64| -> Complex64 { x * x };
-    let df = |x: Complex64| -> Complex64 { 2. * x };
+    let f = |x: Complex64| -> Complex64 { x.powu(3) + 1. };
+    let df = |x: Complex64| -> Complex64 { 3. * x.powu(2) };
 
     for i in 0..s.0 {
         for j in 0..s.1 {
             let z0 = Complex64::from_index([i, j], min_z, max_z, s.0, s.1);
             let res = newton(f, df, z0);
-            let q = quadrant(res);
-
-            out[[i, j, 0]] = q.r;
-            out[[i, j, 1]] = q.g;
-            out[[i, j, 2]] = q.b;
+            root_img[[i, j]] = roots.push_maybe(res);
         }
     }
-
-    let im = array_to_image(out);
-    im.save("plots/out.png").unwrap();
+    println!("{:?}", root_img);
     println!("done")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_cluster() {
+        let mut cluster = Cluster::new();
+
+        let numbers = vec![
+            Complex64::new(0., 0.),
+            Complex64::new(0., 0.001),
+            Complex64::new(0.0001, 0.0001),
+            Complex64::new(1., 0.),
+            Complex64::new(1.0001, 0.),
+        ];
+
+        let mut cls: Vec<usize> = vec![];
+
+        for num in numbers.iter() {
+            let idx = cluster.push(*num);
+            cls.push(idx);
+        }
+
+        assert_eq!(cls, vec![0, 0, 0, 1, 1]);
+        println!("{:?}", cls);
+    }
+
+    #[test]
+    fn test_rem() {
+        println!("{}", -5 % 3);
+        println!("{}", -2 % 3);
+        println!("{}", -5 % 5);
+        println!("{}", -5 % -3);
+        println!("{}", -5 % -5);
+        println!("{}", 5 % -3);
+    }
 }
--- src/viz.rs
+++ src/viz.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use image::RgbImage;
 use ndarray::Array3;
 use num::Complex;
@@ -7,6 +9,52 @@ pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
+}
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+}
+
+// Names for real colors
+pub enum Colors {
+    Black,
+    White,
+    Red,
+    Green,
+    Blue,
+    DarkRed,
+    NavyBlue,
+    DarkBlue,
+    SunYellow,
+}
+
+impl Colors {
+    pub fn value(&self) -> Color {
+        match *self {
+            Colors::Black => Color::new(0, 0, 0),
+            Colors::White => Color::new(1, 1, 1),
+            Colors::Red => Color::new(1, 0, 0),
+            Colors::Green => Color::new(0, 1, 0),
+            Colors::Blue => Color::new(0, 0, 1),
+            // Beautyful Colors
+            Colors::DarkRed => Color::new(175, 0, 0),
+            Colors::NavyBlue => Color::new(0, 159, 255),
+            Colors::DarkBlue => Color::new(51, 122, 183),
+            Colors::SunYellow => Color::new(0, 80, 255),
+        }
+    }
+
+    pub fn from_int(i: isize) -> Color {
+        match i % 4 {
+            0 => Colors::NavyBlue.value(),
+            1 => Colors::NavyBlue.value(),
+            2 => Colors::NavyBlue.value(),
+            3 => Colors::NavyBlue.value(),
+            _ => Colors::Black.value(),
+        }
+    }
 }
 
 pub fn array_to_image(arr: Array3<u8>) -> RgbImage {
@@ -25,24 +73,12 @@ pub fn quadrant<T: Zero + PartialOrd>(z: Option<Complex<T>>) -> Color {
     if z.is_some() {
         let z = z.unwrap();
         match (z.re >= T::zero(), z.im >= T::zero()) {
-            (true, true) => Color {
-                r: 0,
-                g: 159,
-                b: 255,
-            },
-            (false, true) => Color {
-                r: 255,
-                g: 175,
-                b: 0,
-            },
-            (true, false) => Color { r: 175, g: 0, b: 0 },
-            (false, false) => Color {
-                r: 51,
-                g: 122,
-                b: 183,
-            },
+            (true, true) => Colors::NavyBlue.value(),
+            (false, true) => Colors::SunYellow.value(),
+            (true, false) => Colors::DarkRed.value(),
+            (false, false) => Colors::DarkBlue.value(),
         }
     } else {
-        Color { r: 0, g: 0, b: 0 }
+        Colors::Black.value()
     }
 }
